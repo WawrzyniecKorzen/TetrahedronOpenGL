9abdb592605e81d71055d6c3f12f1e295b0a37a0
Wawrzyniec Korzen <wawrzyniec.korzen@gmail.com>
2024-03-15 14:01:03 +0100

    Add project files.

---
+++ TetrahedronOpenGL/src/Application.cpp
@@ -0,0 +1,148 @@
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    Shader ourShader("src/shader.vert", "src/shader.frag"); 
+
+
+    float vertices[] = {
+         -0.5f, -0.228f, -0.204f,  // first face
+         0.5f, -0.228f, -0.204f,     
+         0.0f,  0.0f, 0.612f,     
+         -0.5f, -0.228f, -0.204f,   //second face
+         0.0f, 0.577f, -0.204f,    
+         0.0f,  0.0f, 0.612f,
+         0.5f, -0.228f, -0.204f,    //third face
+         0.0f, 0.577f, -0.204f,    
+         0.0f,  0.0f, 0.612f,
+         -0.5f, -0.228f, -0.204f,  // fourth face
+         0.5f, -0.228f, -0.204f,     
+         0.0f, 0.577f, -0.204f
+    };
+    unsigned int indices[] = {
+        0, 1, 2,
+        3, 4, 5
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+   
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        processInput(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ourShader.use();
+
+
+
+        glm::mat4 model = glm::mat4(1.0f); 
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 projection = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+        
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        
+        ourShader.setMat4("projection", projection);
+
+
+        glBindVertexArray(VAO);
+
+        ourShader.setMat4("projection", projection);
+        ourShader.set4Float("ourColor", 1.0, 0.0, 0.0, 1.0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        ourShader.set4Float("ourColor", 0.0, 1.0, 0.0, 1.0);
+        glDrawArrays(GL_TRIANGLES, 3, 3);
+        ourShader.set4Float("ourColor", 0.0, 0.0, 1.0, 1.0);
+        glDrawArrays(GL_TRIANGLES, 6, 3);
+        ourShader.set4Float("ourColor", 1.0, 1.0, 0.0, 1.0);
+        glDrawArrays(GL_TRIANGLES, 9, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
